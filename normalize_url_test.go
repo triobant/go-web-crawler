3e295b4596e4553c4d7b19cdc1785a614756ed0d
Brian Ott <bott@b1-systems.de>
2024-09-15 23:08:23 +0200

    update: example test for normalize_url

---
+++ normalize_url_test.go
@@ -0,0 +1,27 @@
+func TestNormalizeURL(t *testing.T) {
+	tests := []struct {
+		name          string
+		inputURL      string
+		expected      string
+	}{
+		{
+			name:     "remove scheme",
+			inputURL: "https://blog.boot.dev/path",
+			expected: "blog.boot.dev/path",
+		},
+        // add more test cases here
+	}
+
+	for i, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			actual, err := normalizeURL(tc.inputURL)
+			if err != nil {
+				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
+				return
+			}
+			if actual != tc.expected {
+				t.Errorf("Test %v - %s FAIL: expected URL: %v, actual: %v", i, tc.name, tc.expected, actual)
+			}
+		})
+	}
+}
