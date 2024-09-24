4240f192e9917d28b5585c381de07790fbbc1675
Brian Ott <bott@b1-systems.de>
2024-09-24 10:58:32 +0200

    update: more test cases to check normalizeURL func

--- normalize_url_test.go
+++ normalize_url_test.go
@@ -1,8 +1,16 @@
+package main
+
+import (
+    "strings"
+    "testing"
+)
+
 func TestNormalizeURL(t *testing.T) {
 	tests := []struct {
 		name          string
 		inputURL      string
 		expected      string
+        errorContains string
 	}{
 		{
 			name:     "remove scheme",
@@ -10,19 +18,25 @@ func TestNormalizeURL(t *testing.T) {
 			expected: "blog.boot.dev/path",
 		},
 		{
-			name:     "remove scheme with slash at the end",
+			name:     "remove scheme and trailing slash",
 			inputURL: "https://blog.boot.dev/path/",
 			expected: "blog.boot.dev/path",
 		},
 		{
-			name:     "remove scheme http",
-			inputURL: "http://blog.boot.dev/path",
+			name:     "lowercase capital letters",
+			inputURL: "https://BLOG.boot.dev/PATH",
 			expected: "blog.boot.dev/path",
 		},
 		{
-			name:     "remove scheme http with slash at the end",
-			inputURL: "http://blog.boot.dev/path/",
+			name:     "remove scheme and capitals and trailing slash",
+			inputURL: "http://BLOG.boot.dev/path/",
 			expected: "blog.boot.dev/path",
+		},
+		{
+			name:     "handle invalid URL",
+            inputURL: `:\\invalidURL`,
+			expected: "",
+            errorContains: "couldn't parse URL",
 		},
         // add more test cases here
 	}
@@ -30,10 +44,17 @@ func TestNormalizeURL(t *testing.T) {
 	for i, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
 			actual, err := normalizeURL(tc.inputURL)
-			if err != nil {
+			if err != nil && !strings.Contains(err.Error(), tc.errorContains) {
 				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
 				return
-			}
+			} else if err != nil && tc.errorContains == "" {
+                t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
+                return
+            } else if err == nil && tc.errorContains != "" {
+                t.Errorf("Test %v - '%s' FAIL: unexpected error: '%v', got none.", i, tc.name, tc.errorContains)
+                return
+            }
+
 			if actual != tc.expected {
 				t.Errorf("Test %v - %s FAIL: expected URL: %v, actual: %v", i, tc.name, tc.expected, actual)
 			}
