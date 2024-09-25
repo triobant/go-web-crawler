bf369fb9b34094f7c8b26fcbab02fc36c5dcfd5c
Brian Ott <bott@b1-systems.de>
2024-09-25 22:22:26 +0200

    update: main func to check cli args

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
     "fmt"
+    "os"
 )
 
 func main() {
-    fmt.Println("Hello, World!")
+    argsWithoutProg := os.Args[1:]
+    if len(argsWithoutProg) < 1 {
+        fmt.Println("no website provided")
+    } else if len(argsWithoutProg) > 1 {
+        fmt.Println("too many arguments provided")
+    }
+    fmt.Printf("starting crawl of: %s", argsWithoutProg)
 }
